b254cd42ed4e4292d0b1a82efc1668c5fb34bb63
Ahsan Iqbal <md.ahsan.iqbal@gmail.com>
2024-09-23 11:55:31 +0600

    LangChain Expression Language - RAG example with Runnables

---
+++ lcel_runnable_example.py
@@ -0,0 +1,31 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda, RunnableParallel
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+
+from langchain_community.vectorstores import FAISS
+
+model_name = "llama3.1"
+
+#setting up vectorstore and retriever
+vectorstore = FAISS.from_texts(["I love reading Science Fictions and Detective Novels. My favourite writes in these genres are Agatha Christie and Isaac Asimov"],
+                               embedding = OllamaEmbeddings(model = model_name))
+data_retriever = vectorstore.as_retriever()
+
+#setting up the template with context and question
+template = """
+Answer the question asked from the give context: {context}
+Question: {question}
+"""
+
+#setting up the prompt
+prompt = ChatPromptTemplate.from_template(template)
+
+#setting up the chain
+chain = RunnableParallel(
+    {"context": data_retriever, "question": RunnablePassthrough()}) | prompt | ChatOllama(model = model_name) |StrOutputParser()
+
+
+output = chain.invoke("Who are my favourite writers in which genres?")
+print(output)
